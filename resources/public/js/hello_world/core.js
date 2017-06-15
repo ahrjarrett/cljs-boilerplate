bfdd7d9e5cb4ce4eb77668e75895b3ed99950627
Andrew Jarrett <ahrjarrett@gmail.com>
2017-06-14 23:38:54 -0600

    working through clojureScript unravelled

--- resources/public/js/hello_world/core.js
+++ resources/public/js/hello_world/core.js
@@ -11,6 +11,30 @@ cljs.core.println.call(null,"hello werld");
 hello_world.core.foo = (function hello_world$core$foo(a,b){
 return (a - b);
 });
-cljs.core.println.call(null,hello_world.core.foo.call(null,(10),(20)));
+cljs.core.println.call(null,hello_world.core.foo.call(null,(10),(100)));
+cljs.core.println.call(null,parseInt("222"));
+(new RegExp("^foo$"));
+(new RegExp("^foo$"));
+hello_world.core.re = RegExp("^Clojure");
+cljs.core.println.call(null,hello_world.core.re.test("ClojureScript"));
+Math.sqrt((9));
+Math.sqrt((15));
+console.log("Hello");
+hello_world.core.javascript_object = (function (){var obj29384 = {"taste":"flavor"};
+return obj29384;
+})();
+cljs.core.println.call(null,hello_world.core.javascript_object);
+hello_world.core.javascript_object.taste = "flavors";
+cljs.core.println.call(null,hello_world.core.javascript_object);
+hello_world.core.new_js_obj = ({"country": new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"code","code",1586293142),"FR",new cljs.core.Keyword(null,"name","name",1843675177),"France"], null)});
+cljs.core.println.call(null,hello_world.core.new_js_obj);
+cljs.core.println.call(null,hello_world.core.new_js_obj.country);
+cljs.core.println.call(null,hello_world.core.new_js_obj.country.code);
+hello_world.core.newly_minted_clojure_obj = cljs.core.js__GT_clj.call(null,hello_world.core.new_js_obj);
+cljs.core.println.call(null,[cljs.core.str.cljs$core$IFn$_invoke$arity$1("this is my newly minted clojure object made from js->clj: \n"),cljs.core.str.cljs$core$IFn$_invoke$arity$1(hello_world.core.newly_minted_clojure_obj),cljs.core.str.cljs$core$IFn$_invoke$arity$1("\n"),cljs.core.str.cljs$core$IFn$_invoke$arity$1("and this is the JS object: \n"),cljs.core.str.cljs$core$IFn$_invoke$arity$1(hello_world.core.new_js_obj.country)].join(''));
+cljs.core.println.call(null,new cljs.core.Keyword(null,"country","country",312965309).cljs$core$IFn$_invoke$arity$1(hello_world.core.newly_minted_clojure_obj));
+cljs.core.into_array.call(null,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, ["Bob Dylan","The Birds","The Yardbirds"], null));
+cljs.core.println.call(null,[null,null,null,null,null,null,null,null,null,null]);
+cljs.core.println.call(null,cljs.core.count.call(null,[null,null,null,null,null,null,null,null,null,null,null]));
 
-//# sourceMappingURL=core.js.map?rel=1495582170590
+//# sourceMappingURL=core.js.map?rel=1497505091179
--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -7,6 +7,7 @@
 (defonce conn
   (repl/connect "http://localhost:9000/repl"))
 
+
 (enable-console-print!)
 
 (println "hello werld")
@@ -14,6 +15,100 @@
 (defn foo [a b]
   (- a b))
 
-(println (foo 10 20))
+(println (foo 10 100))
+
+(println (js/parseInt "222"))
+
+;; For RegExp you need to invoke the "new" special form,
+;; which you can do in two ways:
+
+(new js/RegExp "^foo$")
+
+(js/RegExp. "^foo$")
+
+;; The second one is what is recommended.
+
+;; If we want to invoke methods of some object instance,
+;; re use the . first:
+;; (notice how .test is used; usually you test a string
+;; -- can you test a RegExp against an object? --
+;; Here we call the method and pass in the thing we want
+;; to test, followed by the object we're actually testing)
+
+(def re (js/RegExp "^Clojure"))
+(println
+  (.test re "ClojureScript")) ;; => true
+
+
+(.sqrt js/Math 9)
+(js/Math.sqrt 15)
+(js/console.log "Hello")
+
+;; JS-OBJ is a new function:
+(def javascript-object
+  (js-obj "taste" "flavor")) ;; {:taste flavor}
+
+
+;; #JS
+
+;; #js is a reader macro that prepends the argument
+
+(println javascript-object) ;; #js {:taste flavor}
+
+(set! (.-taste javascript-object) "flavors")
+
+(println javascript-object) ;; #js {:taste flavors}
+
+
+;; The problem is that these JS objects cannot access
+;; nested objects
+
+(def new-js-obj #js {:country {:code "FR" :name "France"}})
+
+(println new-js-obj)
+(println (.-country new-js-obj)) ;; => {:code "FR" :name "France"}})
+(println (.-code (.-country new-js-obj))) ;; => nil
+
+;; That's why we need the clj->js and js->clj functions.
+
+(def newly-minted-clojure-obj (js->clj new-js-obj))
+(println (str "this is my newly minted clojure object made from js->clj: \n"
+              newly-minted-clojure-obj
+              "\n"
+              "and this is the JS object: \n"
+              (.-country new-js-obj)
+              ))
+
+;; For some reason this still returns NIL should be "FR"
+(println (:country newly-minted-clojure-obj))
+
+;; INTO-ARRAY behaves as expected:
+
+(into-array ["Bob Dylan" "The Birds" "The Yardbirds"]) ;; => #js [...]
+
+;; We can also preallocate arrays:
+
+(println (make-array 10)) ;; => [nil nil nil nil nil nil nil nil nil nil ]
+(println (count (make-array 11))) ;; => 11
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
